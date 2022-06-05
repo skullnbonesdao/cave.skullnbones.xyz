d888668efafcf555d73b1be65953f98b5eef55c2
skullandbonesdao <skullandbonesdao@gmail.com>
2022-06-05 22:55:02 +0200

    fixed a few links into nowhere

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,23 @@
     >
       <div class="hero-overlay bg-opacity-60"></div>
       <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
+        <div class="max-w-md space-y-10">
           <h1 class="mb-5 text-5xl font-bold">SKULL & BONES</h1>
           <p class="mb-5">
             Skull & Bones is Star Atlas’ 1st and largest piracy DAC
           </p>
-          <button class="btn btn-primary">Go to StarAtlas</button>
+          <a
+            class="btn btn-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://staratlas.com/"
+          >
+            Go to StarAtlas
+          </a>
+          <div>
+            <p>The tools on this site are creation of the SkullnBonesDAO</p>
+            <p>They are build for everyone</p>
+          </div>
         </div>
       </div>
     </div>
