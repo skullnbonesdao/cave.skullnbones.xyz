2ddc9c8e218116e3fac6d630a000762e40d8387b
skullandbonesdao <skullandbonesdao@gmail.com>
2022-06-06 14:43:26 +0200

    readded url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/" + "" + "/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/" + "cave.skullnbones.xyz" + "/"
+      : "/",
 
   configureWebpack: {
     plugins: [
